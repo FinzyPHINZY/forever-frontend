6ff1eef88e331125a18037e3a80e903b1384e2df
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-01-26 15:16:25 +0100

    feat: Implemented Login/Signup Component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,65 @@
+import { useState } from 'react';
+
 const Login = () => {
-  return <div>login page</div>;
+  const [currentState, setCurrentState] = useState('Sign Up');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto gap-4 mt-14 text-gray-800"
+    >
+      <div className="inline-flex items-center gap-2 mb-2 mt-20">
+        <p className="prata-regular text-3xl">{currentState}</p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
+      </div>
+      {currentState === 'Sign Up' && (
+        <input
+          type="text"
+          className="w-full px-3 py-2 border border-gray-800"
+          placeholder="Name"
+          required
+        />
+      )}
+      <input
+        type="email"
+        className="w-full px-3 py-2 border border-gray-800"
+        placeholder="Email"
+        required
+      />
+      <input
+        type="password"
+        className="w-full px-3 py-2 border border-gray-800"
+        placeholder="Password"
+        required
+      />
+      <div className="w-full flex justify-between text-sm mt-[-8px]">
+        <p className="cursor-pointer">Forgot password?</p>
+
+        {currentState === 'Sign Up' ? (
+          <p
+            className="cursor-pointer capitalize"
+            onClick={() => setCurrentState('Login')}
+          >
+            Login here
+          </p>
+        ) : (
+          <p
+            className="cursor-pointer capitalize"
+            onClick={() => setCurrentState('Sign Up')}
+          >
+            Create account
+          </p>
+        )}
+      </div>
+      <button className="bg-black text-white font-light px-8 py-2 mt-4">
+        {currentState === 'Login' ? 'Sign In' : 'Sign Up'}
+      </button>
+    </form>
+  );
 };
 
 export default Login;
